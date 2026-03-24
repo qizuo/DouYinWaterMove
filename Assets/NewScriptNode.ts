78ca5c37e4595e99f0f7a26f1e534de1324e198d
ws <372892246@qq.com>
2026-03-24 20:25:08 +0800

    动起来

--- Assets/NewScriptNode.ts
+++ Assets/NewScriptNode.ts
@@ -21,26 +21,40 @@ export class CustomNodeTS extends BasicScriptNode{
   /**
    * Executed at graph node
    */
-  execute() {
-    // Insert more for your logic
-    // this.trans.localScale = new Vector3f(3,3,3);
-    // this.trans.localPosition = new Vector3f(4,-4,-40);
-    this.trans.localPosition = new Vector3f(0,-4,-40);
-    let endPos = new Vector3f(this.trans.localPosition.x+5,this.trans.localPosition.y,this.trans.localPosition.z);
-    const tween1 = new TWEEN.Tween(this.trans).to(endPos, 500).easing(TWEEN.Easing.Quadratic.InOut);
-    tween1.start();
-    
-    this.info = "success4";
-    this.vecInfo = this.trans.localPosition; 
-    }
+  @input()
+  isStartEvent: boolean;
 
+  //True When Start Event is triggered, Otherwise False
+  @input()
+  isUpdateEvent: boolean;
+
+  execute() {
+
+   if (this.isStartEvent && !this.isUpdateEvent) {
+        this.isStartEvent = false;
+        this.onStart();
+      }
+      if (this.isUpdateEvent) {
+        this.isUpdateEvent = false;
+        this.onUpdate();
+      }
+  }
+
+  onStart() {
+    const control1 = this.trans.localPosition;
+    const control2 = new Vector3f(4,0,-40);
+    let posUpdateFunc = (pos: APJS.Vector3f, _elapsedTime: number) => {
+      this.trans.localPosition = pos;
+    };
+
+    const tween = new TWEEN.Tween(this.trans.localPosition).onUpdate(posUpdateFunc).to(control2, 1000).start();
+  }
+
+  onUpdate() {
+    TWEEN.update();
+  }
+  
     // transUpdate(pos: APJS.Vector3f, _elapsedTime: number){
     //   this.trans.localPosition = pos;
     // }
-   update(deltaTime: number) {
-    TWEEN.update();
-    this.frameCount++;
-    this.info = "success5."+this.frameCount;
-    this.trans.localPosition = new Vector3f(0,-4,-40);
-    }
 }
